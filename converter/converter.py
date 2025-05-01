e7e46d80efc8b3c90d78add3df95f40dd64e7567
Fikri Aqsha <m.fikriaqsha@gmail.com>
2025-05-01 14:00:11 +0700

    add converter module

---
+++ converter/converter.py
@@ -0,0 +1,14 @@
+def celcius_to_kelvin(value):
+    return value + 273.15
+
+def fahrenheit_to_celcius(value):
+    return (value - 32) * (5 / 9)
+
+def fahrenheit_to_kelvin(value):
+    return (value - 32) * (5 / 9) + 273.15
+
+def kelvin_to_celcius(value):
+    return value - 273.15
+
+def kelvin_to_fahrenheit(value):
+    return (value - 273.15) * (9 / 5) + 32
