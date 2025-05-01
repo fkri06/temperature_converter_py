76ad6e0ba676d9bec81bc0dbebcaf2f224d14676
Fikri Aqsha <m.fikriaqsha@gmail.com>
2025-05-01 13:04:28 +0700

    add dropdown menu, input entry and convert button

--- interface/MainUI.py
+++ interface/MainUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 class TemperatureConverterInterface:
 
@@ -16,7 +17,33 @@ class TemperatureConverterInterface:
         from_temperature_label = Label(window, text="From", font=("Arial", 14, "normal"))
         from_temperature_label.grid(sticky="w", row=1, column=0)
 
+        # Drop down menu
+        self.first_choice = DropDownMenu(window, 2, 0, 1) 
+
         to_temperature_label = Label(window, text="To", font=("Arial", 14, "normal"))
         to_temperature_label.grid(sticky="w", row=1, column=1)
 
+        self.second_choice = DropDownMenu(window, 2, 1, 2)
+
+        # Input entry and button
+
+        input_value = Entry()
+        input_value.grid(sticky="w", row=4, column=0, pady=35)
+        input_value.insert(END, "Input the value here...")
+
+        button = Button(text="Convert", command=self.show_value, width=10)
+        button.grid(sticky="w", row=5, column=0)
+
         window.mainloop()
+        
+class DropDownMenu:
+    def __init__(self, window: Tk, row: int, column: int, rowspan: int):
+        self.options = ["Celcius", "Fahrenheit", "Kelvin"]
+        self.selected_option = StringVar(window)
+        self.selected_option.set("Choose temperature")
+        self.drop_down = OptionMenu(window, self.selected_option, *self.options, command=self.set_menu_value)
+        self.value = ""
+        self.drop_down.grid(sticky="w", row=row, column=column, rowspan=rowspan)
+    
+    def set_menu_value(self, value):
+        self.value = value
