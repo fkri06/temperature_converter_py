c2c46d406886073d70aa6a52e75f25889982e2bb
Fikri Aqsha <m.fikriaqsha@gmail.com>
2025-05-01 09:02:23 +0700

    adding basic label layout

--- interface/MainUI.py
+++ interface/MainUI.py
@@ -1,10 +1,23 @@
 from tkinter import *
+from constants import Constants
 
 class TemperatureConverterInterface:
 
     def __init__(self):
         window = Tk()
         window.title("Temperature Converter")
-        window.minsize(width=500, height=500)
+        window.minsize(width=640, height=350)
         window.config(padx=20, pady=20)
+
+        # Labels
+        app_label = Label(window, text="Convert Temperature", font=("Times New Roman", 24, "bold"))
+        app_label.grid(sticky="w", row=0, column=0)
+        app_label.config(pady=10, padx=10)
+
+        from_temperature_label = Label(window, text="From", font=("Arial", 14, "normal"))
+        from_temperature_label.grid(sticky="w", row=1, column=0)
+
+        to_temperature_label = Label(window, text="To", font=("Arial", 14, "normal"))
+        to_temperature_label.grid(sticky="w", row=1, column=1)
+
         window.mainloop()
